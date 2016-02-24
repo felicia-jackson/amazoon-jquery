96f319227189ac9cc3e367cd5019de95e975e70e
felicia-jackson <feliciacj@rocketmail.com>
2016-02-23 19:23:13 -0600

    Preparing to publish

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -24,5 +24,62 @@ $(document).ready(function(){
     ];
 
     // Get started here!
-
+    // var newElement = $('<div />').text(products[0].name)
+    // $('body').append(newElement)
+    // var newElement = $('<span />').text(products[0].price)
+    // $('.cart').append(newElement)
+    var newElement = $( $('template').html() )
+    $('.manufacturer',newElement).text(products[0].name)
+    $('.cart').append(newElement)
   });
+var updateView = function(){
+  var productsInCart = $('.cart .product');
+  var productsSaved = $(.saved .product);
+
+  if(productsInCart.length === 0){
+    $('.cart .empty').show();
+    $('')
+  } else {
+    $('.cart .empty').hide();
+  }
+  if(productsSaved.length === 0){
+    $('.cart .empty').show();
+  } else {
+    $('.cart .empty').hide();
+  }
+var total = 0;
+  $(productsInCart).each(fuction()){
+    total += Number($(this).data('price'));
+  }
+};
+$('cart-total span').text('$' + total.toFixed(2));
+
+var productTemplate = $('template#product').html();
+console.log(productTemplate)
+
+for(var i= 0; i <products.length; i++){
+  var newProduct = $(productTemplate);
+  var pData= products[i];
+
+  $('.content h2', newProduct).text(products[i].name);
+  var manufacturerSpan = $('<span />').addClass('manufacturer').text('by' + products[i].manufacturer)
+  $('.content h2',newProduct ).append(manufacturerSpan);
+  $('.content h3',newProduct ).text(products[i].description);
+  $('.image img', newProduct).attr('src', products[i].imageSrc);
+  $('.price', '.mobile-price').text('$'+ products[i].price.toFixed(2));
+  $(newProduct).data('price', products[i].price);
+
+
+  $('.cart').append(newProduct)
+}
+
+updateView();
+
+$('.product'.move').on('click', function()){
+  if($(this).parents().eq(4).hasClass('cart')){
+  $(this).parents(.eq(3).prependTo('.saved');
+    $(this).text('Add to cart');
+  } else {
+
+  }
+}
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
   // Get started here!
+
+  $('.order-form').on('submit', function(event){
+    event.preventDefault();
+  $('input[required]').parent().removeClass('has-error');
+  $('.error-message').hide();
+    // 1) Grab all of the required inputs
+    var requiredFields = $('input[required]');
+    // 2) Loop over each of those inputs and check if they're empty (.val().length)
+    var isValid = true;
+    $(requiredFields).each(function(){
+      // This function runs for every input
+      if ($(this).val().length === 0) {
+        isValid = false;
+        $(this).parent().addClass('has-error')
+        $('.error-message').show()
+      } else {
+        $(this).parent().removeClass('has-error');
+      }
+
+    });
+    if (isValid){
+      $('.error-message').hide()
+      alert('You completed the form');
+    } else {
+      $('.error-message').show();
+    }
+
+      // 3) If the current input is empty, consider the form to be invalid and give .has-error class to the input
+
+    // 4) IF there's one input with the class .has-error, we can assume that the form is invalid and we can show a message to the user
+  });
 });
